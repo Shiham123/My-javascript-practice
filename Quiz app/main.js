406430d0e10aa9ef954ad8ef3c93faf9ffc131fd
unknown <s36651786@gmail.com>
2023-04-20 10:59:18 +0600

    be patient ALLAH is with you hold on go steop ahead little by little and doesn't matter how step small is matter is that step if it very little

--- Quiz app/main.js
+++ Quiz app/main.js
@@ -3,7 +3,8 @@ const startQuizEl = document.getElementById('start-quiz'),
   boxTwoEl = document.getElementById('box-two'),
   exitBtnEl = document.getElementById('exitBtn'),
   continueBtnEl = document.getElementById('continueBtn'),
-  boxThreeEl = document.getElementById('box-three');
+  boxThreeEl = document.getElementById('box-three'),
+  boxFourEl = document.getElementById('box-four');
 
 const quizQuestionEl = document.getElementById('quizQuestion'),
   nextQuizEl = document.getElementById('nextQuiz');
@@ -35,7 +36,8 @@ function incrementQuiz() {
     currentQuestion++;
     showQuizData(currentQuestion);
   } else {
-    console.log('decrement');
+    boxThreeEl.style.display = 'none';
+    boxFourEl.style.display = 'block';
   }
 }
 
@@ -43,15 +45,19 @@ function showQuizData() {
   quizQuestionEl.innerHTML = `
   <h1>${quizData[currentQuestion].question}</h1>
     <ol>
-      <li>
-      ${quizData[currentQuestion].options[0]}
-        <span><i class="fa-solid fa-check"></i></span>
-      </li>
+      <li>${quizData[currentQuestion].options[0]}</li>
       <li>${quizData[currentQuestion].options[1]}</li>
       <li>${quizData[currentQuestion].options[2]}</li>
       <li>${quizData[currentQuestion].options[3]}</li>
     </ol>
   `;
+
+  let addIcon = quizQuestionEl.querySelectorAll('li');
+  addIcon.forEach((e) => {
+    e.addEventListener('click', (e) => {
+      console.log(e);
+    });
+  });
 }
 
 showQuizData();
