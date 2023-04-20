619b91debef8444277bd2680cdac1487956d0f43
unknown <s36651786@gmail.com>
2023-04-20 10:38:22 +0600

    My MALIK is AR RAHAMN and AR RAHIM

--- Quiz app/main.js
+++ Quiz app/main.js
@@ -5,6 +5,9 @@ const startQuizEl = document.getElementById('start-quiz'),
   continueBtnEl = document.getElementById('continueBtn'),
   boxThreeEl = document.getElementById('box-three');
 
+const quizQuestionEl = document.getElementById('quizQuestion'),
+  nextQuizEl = document.getElementById('nextQuiz');
+
 startQuizEl.addEventListener('click', () => {
   boxOneEl.style.display = 'none';
   boxTwoEl.style.display = 'block';
@@ -20,3 +23,35 @@ continueBtnEl.addEventListener('click', () => {
   boxTwoEl.style.display = 'none';
   boxThreeEl.style.display = 'block';
 });
+
+nextQuizEl.addEventListener('click', () => {
+  incrementQuiz();
+});
+
+let currentQuestion = 0;
+
+function incrementQuiz() {
+  if (currentQuestion < quizData.length - 1) {
+    currentQuestion++;
+    showQuizData(currentQuestion);
+  } else {
+    console.log('decrement');
+  }
+}
+
+function showQuizData() {
+  quizQuestionEl.innerHTML = `
+  <h1>${quizData[currentQuestion].question}</h1>
+    <ol>
+      <li>
+      ${quizData[currentQuestion].options[0]}
+        <span><i class="fa-solid fa-check"></i></span>
+      </li>
+      <li>${quizData[currentQuestion].options[1]}</li>
+      <li>${quizData[currentQuestion].options[2]}</li>
+      <li>${quizData[currentQuestion].options[3]}</li>
+    </ol>
+  `;
+}
+
+showQuizData();
