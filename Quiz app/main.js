6e097017c69886ca839b4e5f6eb89c2630256614
unknown <s36651786@gmail.com>
2023-04-27 10:22:17 +0600

    Glory to be ALLAH, moving forward

--- Quiz app/main.js
+++ Quiz app/main.js
@@ -7,13 +7,22 @@ const startQuizBtnEl = document.getElementById('start-quiz'),
   exitBtnEl = document.getElementById('exitBtn'),
   continueBtnEl = document.getElementById('continueBtn'),
   nextQuizBtnEl = document.getElementById('nextQuiz'),
-  quitQuizBtnEl = document.getElementById('quitQuiz');
+  quitQuizBtnEl = document.getElementById('quitQuiz'),
+  replyQuizBtnEl = document.getElementById('replyQuiz');
 
 const questionEl = document.getElementById('question'),
   optionEl = document.getElementById('option'),
-  totalEl = document.getElementById('total');
+  totalEl = document.getElementById('total'),
+  timerCountEl = document.getElementById('seconds'),
+  timerLineEl = document.getElementById('timer-lines'),
+  resultContentEl = document.getElementById('result-content');
 
 let questionCount = 0;
+let timerCounter;
+let timerValue = 15;
+let lineCounter;
+let lineValue = 0;
+let userScore = 0;
 
 startQuizBtnEl.addEventListener('click', () => {
   boxOneEl.style.display = 'none';
@@ -29,21 +38,43 @@ continueBtnEl.addEventListener('click', () => {
   boxOneEl.style.display = 'none';
   boxTwoEl.style.display = 'none';
   boxThreeEl.style.display = 'block';
+
   showQuiz(questionCount);
+  startTimer(timerValue);
+  startTimerLine(lineValue);
 });
 
 nextQuizBtnEl.addEventListener('click', () => {
   if (questionCount < quizData.length - 1) {
     questionCount++;
     showQuiz(questionCount);
+
+    clearInterval(timerCounter);
+    startTimer(timerValue);
+
+    clearInterval(lineCounter);
+    startTimerLine(lineValue);
   } else {
     boxThreeEl.style.display = 'none';
     boxFourEl.style.display = 'block';
+    showResult(userScore);
   }
 });
 
 quitQuizBtnEl.addEventListener('click', () => {
   location.reload();
+});
+
+replyQuizBtnEl.addEventListener('click', () => {
+  boxFourEl.style.display = 'none';
+  boxTwoEl.style.display = 'block';
+
+  questionCount = 0;
+  userScore = 0;
+  if (questionCount < quizData.length - 1) {
+    showQuiz(questionCount);
+    showResult(userScore);
+  }
 });
 
 function showQuiz(index) {
@@ -63,6 +94,9 @@ function showQuiz(index) {
 }
 
 function selectedOption(answer) {
+  clearInterval(lineCounter);
+  clearInterval(timerCounter);
+
   let userAnswer = answer.textContent;
   let correctAnswer = quizData[questionCount].correct;
 
@@ -70,6 +104,8 @@ function selectedOption(answer) {
     crossIcon = `<i class="fa-solid fa-times"></i>`;
 
   if (userAnswer === correctAnswer) {
+    userScore += 1;
+
     answer.classList.add('correct');
     answer.insertAdjacentHTML('beforeend', checkIcon);
   } else {
@@ -86,3 +122,49 @@ function selectedOption(answer) {
   }
   nextQuizBtnEl.style.display = 'block';
 }
+
+function startTimer(timer) {
+  timerCountEl.textContent = timer;
+
+  timerCounter = setInterval(() => {
+    timerCountEl.textContent = timer;
+    timer--;
+
+    if (timer < 0) {
+      clearInterval(timerCounter);
+
+      for (let i = 0; i < optionEl.children.length; i++) {
+        optionEl.children[i].classList.add('disabled');
+      }
+
+      totalEl.innerHTML = '<p>Please reload the page</p>';
+    }
+
+    if (timer < 9) {
+      timerCountEl.textContent = 0 + timerCountEl.textContent;
+    }
+  }, 1000);
+}
+
+function startTimerLine(line) {
+  lineCounter = setInterval(() => {
+    timerLineEl.style.width = line + 'px';
+    line++;
+
+    if (line > 500) {
+      clearInterval(lineCounter);
+    }
+  }, 30);
+}
+
+function showResult(score) {
+  if (score < 3) {
+    resultContentEl.innerHTML = `<h3>Not good, Your result is ${score} Out Of ${quizData.length}</h3>`;
+  } else if (score < 5) {
+    resultContentEl.innerHTML = `<h3>Your must be improved, Your result is ${score} out of ${quizData.length}</h3>`;
+  } else {
+    resultContentEl.innerHTML = `<h3>Excellent, Your are good, Your result is ${score} Out of ${quizData.length}</h3>`;
+  }
+}
+
+showQuiz(questionCount);
