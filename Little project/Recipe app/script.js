8fdabbd25dfa8a65ac18a6b04139ecfd74aa18dd
unknown <s36651786@gmail.com>
2023-04-30 22:49:41 +0600

    today limit is over API

--- Little project/Recipe app/script.js
+++ Little project/Recipe app/script.js
@@ -1,11 +1,2 @@
-const apiKey = '275d58779ccf4e22af03e792e8819fff';
-const recipeListEl = document.getElementById('recipe-list');
-
-function getRecipes() {
-  fetch(`https://api.spoonacular.com/recipes/random?number=10&apiKey=${apiKey}`)
-    .then((Response) => Response.json())
-    .then((data) => console.log(data))
-    .catch((error) => console.log(error));
-}
-
-getRecipes();
+// 275d58779ccf4e22af03e792e8819fff
+// https://api.spoonacular.com/recipes/random?number=10&apiKey=${API_KEY}
