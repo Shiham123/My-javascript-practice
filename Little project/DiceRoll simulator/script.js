40cba5be46c0dae3711b4c717bf15dc9c73d4d6f
unknown <s36651786@gmail.com>
2023-05-01 19:46:01 +0600

    rolling it

---
+++ Little project/DiceRoll simulator/script.js
@@ -0,0 +1,52 @@
+const diceEl = document.getElementById('dice'),
+  buttonEl = document.getElementById('roll-button'),
+  itemEl = document.getElementById('roll-history');
+
+let storeDiceRoll = [];
+
+function rollDice() {
+  const rollResult = Math.floor(Math.random() * 6) + 1;
+  const diceFace = getDiceFace(rollResult);
+  diceEl.innerHTML = diceFace;
+  storeDiceRoll.push(rollResult);
+  updateRollHistory();
+}
+
+function updateRollHistory() {
+  itemEl.innerHTML = '';
+  for (let i = 0; i < storeDiceRoll.length; i++) {
+    const listItem = document.createElement('li');
+    listItem.innerHTML = `Roll ${i + 1} : <span>${getDiceFace(
+      storeDiceRoll[i]
+    )}</span>`;
+    itemEl.appendChild(listItem);
+  }
+}
+
+function getDiceFace(rollResult) {
+  switch (rollResult) {
+    case 1:
+      return '&#9856';
+    case 2:
+      return '&#9857';
+    case 3:
+      return '&#9858';
+    case 4:
+      return '&#9859';
+    case 5:
+      return '&#9860';
+    case 6:
+      return '&#9861';
+    default:
+      return '';
+  }
+}
+
+buttonEl.addEventListener('click', () => {
+  diceEl.classList.add('roll-animation');
+
+  setTimeout(() => {
+    diceEl.classList.remove('roll-animation');
+    rollDice();
+  }, 1000);
+});
