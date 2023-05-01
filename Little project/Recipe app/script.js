e43674b378113e4b0c63175e75049022c3a25b50
unknown <s36651786@gmail.com>
2023-05-01 09:06:38 +0600

    now understand everything help of ALLAH

--- Little project/Recipe app/script.js
+++ Little project/Recipe app/script.js
@@ -1,2 +1,41 @@
 // 275d58779ccf4e22af03e792e8819fff
 // https://api.spoonacular.com/recipes/random?number=10&apiKey=${API_KEY}
+
+const API_KEY = '275d58779ccf4e22af03e792e8819fff';
+const recipeListEl = document.getElementById('recipe-list');
+
+function displayRecipes(recipesValue) {
+  recipesValue.forEach((recipe) => {
+    const recipeItemEl = document.createElement('li');
+    recipeItemEl.classList.add('recipe-item');
+
+    const recipeTitleEl = document.createElement('h2');
+    recipeTitleEl.innerText = recipe.title;
+
+    const recipeIngredientEl = document.createElement('p');
+    recipeIngredientEl.innerHTML = `<strong>Ingredients : </strong> ${recipe.extendedIngredients
+      .map((ingredient) => ingredient.original)
+      .join(', ')}`;
+
+    recipeListEl.appendChild(recipeItemEl);
+    recipeItemEl.appendChild(recipeTitleEl);
+    recipeItemEl.appendChild(recipeIngredientEl);
+  });
+}
+
+async function getRecipes() {
+  const response = await fetch(
+    `https://api.spoonacular.com/recipes/random?number=10&apiKey=${API_KEY}`
+  );
+
+  const data = await response.json();
+
+  return data.recipes;
+}
+
+async function initialRecipes() {
+  const recipes = await getRecipes();
+  displayRecipes(recipes);
+}
+
+initialRecipes();
