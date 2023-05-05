e495d217ce96863a51159b4657728f99bd2467b7
unknown <s36651786@gmail.com>
2023-05-05 18:17:26 +0600

    running tempareture project

--- Little project/Temperature converter/script.js
+++ Little project/Temperature converter/script.js
@@ -2,59 +2,26 @@ const celsiusEl = document.getElementById('celsius'),
   fahrenheitEl = document.getElementById('fahrenheit'),
   kelvinEl = document.getElementById('kelvin');
 
-let celsiusValue, fahrenheitValue, kelvinValue;
-
 function computeTemp(event) {
-  const currentValue = +event.target.value;
+  const currentValue = Number(event.target.value);
 
   switch (event.target.name) {
     case 'celsius':
-      kelvinEl.value = (currentValue + 273.32).toFixed(2);
       fahrenheitEl.value = (currentValue * 1.8 + 32).toFixed(2);
-      getClearValue(event.target.name);
+      kelvinEl.value = (currentValue + 273.15).toFixed(2);
       break;
 
     case 'fahrenheit':
       celsiusEl.value = ((currentValue - 32) / 1.8).toFixed(2);
-      kelvinEl.value = ((currentValue - 32) / 1.8 + 273.32).toFixed(2);
-      getClearValue(event.target.name);
+      kelvinEl.value = (((currentValue + 459.67) * 5) / 9).toFixed(2);
       break;
 
     case 'kelvin':
-      celsiusEl.value = (currentValue - 273.32).toFixed(2);
-      fahrenheitEl.value = ((currentValue - 273.32) * 1.8 + 32).toFixed(2);
-      getClearValue(event.target.name);
+      celsiusEl.value = (currentValue - 273.15).toFixed(2);
+      fahrenheitEl.value = (currentValue * 1.8 - 459.67).toFixed(2);
       break;
 
     default:
-      break;
-  }
-}
-
-function getClearValue(value) {
-  switch (value) {
-    case 'celsius':
-      clearTimeout(celsiusValue);
-      celsiusValue = setTimeout(() => {
-        kelvinEl.value = '';
-        fahrenheitEl.value = '';
-      }, 2000);
-      break;
-
-    case 'fahrenheit':
-      clearTimeout(fahrenheitValue);
-      fahrenheitValue = setTimeout(() => {
-        celsiusEl.value = '';
-        kelvinEl.value = '';
-      }, 2000);
-      break;
-
-    case 'kelvin':
-      clearTimeout(kelvinValue);
-      kelvinValue = setTimeout(() => {
-        celsiusEl.value = '';
-        fahrenheitEl.value = '';
-      }, 2000);
       break;
   }
 }
