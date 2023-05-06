f8139c4c6b7310f28739d1b70d6061ba066078b5
unknown <s36651786@gmail.com>
2023-05-06 22:57:33 +0600

    running

--- Little project/Step progress bar/script.js
+++ Little project/Step progress bar/script.js
@@ -2,57 +2,28 @@ const nextBtnEl = document.getElementById('next'),
   prevBtnEl = document.getElementById('prev');
 
 const progressEl = document.querySelector('.progress-bar-front'),
-  stepEl = document.querySelectorAll('.step');
+  stepsEl = document.querySelectorAll('.step');
 
-let currentChecked = 1;
+let currentNumber = 1;
 
 nextBtnEl.addEventListener('click', () => {
-  currentChecked++;
-  if (currentChecked > stepEl.length) {
-    currentChecked = stepEl.length;
+  currentNumber++;
+  if (currentNumber > stepsEl.length - 1) {
+    currentNumber = stepsEl.length;
   }
+
   updateProgressBar();
 });
 
 prevBtnEl.addEventListener('click', () => {
-  currentChecked--;
-  if (currentChecked < 1) {
-    currentChecked = 1;
+  currentNumber--;
+  if (currentNumber < 1) {
+    currentNumber = 1;
   }
+
   updateProgressBar();
 });
 
 function updateProgressBar() {
-  stepEl.forEach((item, index) => {
-    if (index < currentChecked) {
-      item.classList.add('checked');
-      item.innerHTML = `
-      <i class="fas fa-check"></i>
-      <small>${
-        index === 0
-          ? 'Start'
-          : index === stepEl.length - 1
-          ? 'Final'
-          : 'Step ' + index
-      }</small>
-      `;
-    } else {
-      item.classList.remove('checked');
-      item.innerHTML = `<i class="fas fa-times"></i>`;
-    }
-  });
-
-  const checkNumber = document.querySelectorAll('.checked');
-
-  progressEl.style.width =
-    ((checkNumber.length - 1) / (stepEl.length - 1)) * 100 + '%';
-
-  if (currentChecked === 1) {
-    prevBtnEl.disabled = true;
-  } else if (currentChecked === stepEl.length) {
-    nextBtnEl.disabled = true;
-  } else {
-    prevBtnEl.disabled = false;
-    nextBtnEl.disabled = false;
-  }
+  stepsEl.forEach((item, index) => {});
 }
