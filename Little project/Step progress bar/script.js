4240b826ddc7b9db528a0b8a3832103b41d28d15
unknown <s36651786@gmail.com>
2023-05-06 20:23:08 +0600

    running

--- Little project/Step progress bar/script.js
+++ Little project/Step progress bar/script.js
@@ -4,7 +4,7 @@ const nextBtnEl = document.getElementById('next'),
 const progressEl = document.querySelector('.progress-bar-front'),
   stepEl = document.querySelectorAll('.step');
 
-let currentChecked = 0;
+let currentChecked = 1;
 
 nextBtnEl.addEventListener('click', () => {
   currentChecked++;
@@ -17,6 +17,17 @@ nextBtnEl.addEventListener('click', () => {
 
 function updateProgressBar() {
   stepEl.forEach((item, index) => {
-    console.log(item);
+    if (index < currentChecked) {
+      item.classList.add('checked');
+      item.innerHTML = `<i class="fas fa-check"></i> <small>${
+        index === 0
+          ? 'Start'
+          : index === item.length - 1
+          ? 'Final'
+          : 'Step' + index
+      }</small>`;
+    } else {
+      item.classList.remove('checked');
+    }
   });
 }
