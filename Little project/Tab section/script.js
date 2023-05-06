561a69bb5128fc4321bbc42cebb8e1ae7fe9ee85
unknown <s36651786@gmail.com>
2023-05-06 10:59:22 +0600

    tabs project complete

--- Little project/Tab section/script.js
+++ Little project/Tab section/script.js
@@ -1,3 +1,22 @@
-const tabsEl = document.querySelector('.tabs'),
-  btnEl = document.querySelectorAll('.button'),
-  contentEl = document.querySelectorAll('.contents');
+const tabsEl = document.querySelector('.tabs');
+const btnEl = document.querySelectorAll('.button');
+const contentEl = document.querySelectorAll('.content');
+
+tabsEl.addEventListener('click', (e) => {
+  const id = e.target.dataset.id;
+
+  if (id) {
+    btnEl.forEach((btn) => {
+      btn.classList.remove('live');
+    });
+    e.target.classList.add('live');
+
+    contentEl.forEach((content) => {
+      if (content.getAttribute('id') !== id) {
+        content.classList.remove('live');
+      } else {
+        content.classList.add('live');
+      }
+    });
+  }
+});
