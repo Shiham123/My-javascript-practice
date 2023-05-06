18020a48f1fcdf9fad91fc33d202689419c1cbcb
unknown <s36651786@gmail.com>
2023-05-06 21:24:39 +0600

    adding javascript

--- Little project/Step progress bar/script.js
+++ Little project/Step progress bar/script.js
@@ -11,7 +11,14 @@ nextBtnEl.addEventListener('click', () => {
   if (currentChecked > stepEl.length) {
     currentChecked = stepEl.length;
   }
+  updateProgressBar();
+});
 
+prevBtnEl.addEventListener('click', () => {
+  currentChecked--;
+  if (currentChecked < 1) {
+    currentChecked = 1;
+  }
   updateProgressBar();
 });
 
@@ -19,15 +26,33 @@ function updateProgressBar() {
   stepEl.forEach((item, index) => {
     if (index < currentChecked) {
       item.classList.add('checked');
-      item.innerHTML = `<i class="fas fa-check"></i> <small>${
+      item.innerHTML = `
+      <i class="fas fa-check"></i>
+      <small>${
         index === 0
           ? 'Start'
-          : index === item.length - 1
+          : index === stepEl.length - 1
           ? 'Final'
-          : 'Step' + index
-      }</small>`;
+          : 'Step ' + index
+      }</small>
+      `;
     } else {
       item.classList.remove('checked');
+      item.innerHTML = `<i class="fas fa-times"></i>`;
     }
   });
+
+  const checkNumber = document.querySelectorAll('.checked');
+
+  progressEl.style.width =
+    ((checkNumber.length - 1) / (stepEl.length - 1)) * 100 + '%';
+
+  if (currentChecked === 1) {
+    prevBtnEl.disabled = true;
+  } else if (currentChecked === stepEl.length) {
+    nextBtnEl.disabled = true;
+  } else {
+    prevBtnEl.disabled = false;
+    nextBtnEl.disabled = false;
+  }
 }
