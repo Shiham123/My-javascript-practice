7d8032d18229452fa375f3cb9dcf3ea76278de42
unknown <s36651786@gmail.com>
2023-05-10 09:23:58 +0600

    another commit

--- Little project/Note app/script.js
+++ Little project/Note app/script.js
@@ -1,37 +1,22 @@
 const btnEl = document.getElementById('btn'),
-  appBtnEl = document.getElementById('app');
+  appEl = document.getElementById('app');
 
 btnEl.addEventListener('click', addNote);
 
-function createNoteEl(id, content) {
+function addNote() {
+  const randomNoteNumber = Math.floor(Math.random() * 10000);
+  const noteObj = {
+    id: randomNoteNumber,
+    content: '',
+  };
+  const noteEl = createElement(noteObj.id, noteObj.content);
+  appEl.insertBefore(noteEl, btnEl);
+}
+
+function createElement(id, content) {
   const element = document.createElement('textarea');
   element.classList.add('note');
   element.placeholder = 'Empty note';
   element.value = content;
-
-  element.addEventListener('dblclick', () => {
-    const warning = confirm('Do you want to delete this note?');
-    if (warning) {
-      deleteNote(id, element);
-    }
-  });
-
-  element.addEventListener('input', () => {
-    updateNote(id, element.value);
-  });
-
   return element;
-}
-
-function deleteNote() {}
-
-function updateNote() {}
-
-function addNote() {
-  const noteObj = {
-    id: Math.floor(Math.random() * 100000),
-    content: '',
-  };
-  const noteEl = createNoteEl(noteObj.id, noteObj.content);
-  appBtnEl.insertBefore(noteEl, btnEl);
 }
