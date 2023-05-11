54f4fb16e651da17266fbac16e5a6cff554993ea
unknown <s36651786@gmail.com>
2023-05-11 12:13:24 +0600

    img slider

--- Little project/Img slider/script.js
+++ Little project/Img slider/script.js
@@ -4,16 +4,21 @@ const nextIconEl = document.querySelector('.next'),
   imgContainerEl = document.querySelector('.image-container');
 
 let currentImg = 0;
+let timeOut;
 
 nextIconEl.addEventListener('click', () => {
   currentImg++;
+  clearTimeout(timeOut);
   updateImg();
 });
 
 prevIconEl.addEventListener('click', () => {
   currentImg--;
+  clearTimeout(timeOut);
   updateImg();
 });
+
+updateImg()
 
 function updateImg() {
   if (currentImg > imgEl.length) {
@@ -21,5 +26,11 @@ function updateImg() {
   } else if (currentImg < 1) {
     currentImg = imgEl.length;
   }
+
   imgContainerEl.style.transform = `translate(-${(currentImg - 1) * 500}px)`;
+
+  timeOut = setTimeout(() => {
+    currentImg++;
+    updateImg();
+  }, 1000);
 }
