d34f169fd3317edc2287b5d91c34dd3b9de20624
unknown <s36651786@gmail.com>
2023-05-11 10:30:45 +0600

    calendar app

--- Little project/Month Celender/script.js
+++ Little project/Month Celender/script.js
@@ -2,9 +2,9 @@ const monthEl = document.querySelector('.date > h1'),
   fullDateEl = document.querySelector('.date > p'),
   daysEl = document.querySelector('.days');
 
-const monthIndex = new Date().getMonth(),
-  lastDay = new Date(new Date().getFullYear(), monthIndex + 1, 0),
-  firstDay = new Date(new Date().getFullYear(), monthIndex + 1, 1).getDay();
+const monthsIndex = new Date().getMonth(),
+  lastDay = new Date(new Date().getFullYear(), monthsIndex + 1, 0).getDate(),
+  firstDay = new Date(new Date().getFullYear(), monthsIndex, 1).getDay() - 1;
 
 const months = [
   'January',
@@ -13,9 +13,29 @@ const months = [
   'April',
   'May',
   'June',
+  'July',
   'August',
   'September',
   'October',
   'November',
   'December',
 ];
+
+monthEl.innerText = months[monthsIndex];
+fullDateEl.innerText = new Date().toDateString();
+
+let days = '';
+
+for (let index = firstDay; index > 0; index--) {
+  days += `<div class="empty">${index}</div>`;
+}
+
+for (let index = 1; index <= lastDay; index++) {
+  if (index === new Date().getDate()) {
+    days += `<div class="today">${index}</div>`;
+  } else {
+    days += `<div>${index}</div>`;
+  }
+}
+
+daysEl.innerHTML = days;
