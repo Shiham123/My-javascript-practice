14c83f289ea268d888d8f8e5b271179fa40b5f17
unknown <s36651786@gmail.com>
2023-05-13 10:15:36 +0600

    dictionary app running

--- Little project/English dictonary/script.js
+++ Little project/English dictonary/script.js
@@ -2,4 +2,28 @@ const inputEl = document.getElementById('input'),
   infoTextEl = document.getElementById('info-text'),
   meaningContainerEl = document.getElementById('meaning-container'),
   titleEl = document.getElementById('title'),
-  meaningEl = document.getElementById('meaning');
+  meaningEl = document.getElementById('meaning'),
+  audioEl = document.getElementById('audio');
+
+inputEl.addEventListener('keyup', (event) => {
+  if (event.key === 'Enter') {
+    dictionaryCreate(event.target.value);
+  }
+});
+
+async function dictionaryCreate(word) {
+  try {
+    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`,
+      resultElement = await fetch(url).then((data) => data.json());
+
+    if (resultElement.title) {
+      console.log('find it');
+    } else {
+    }
+  } catch (error) {
+    console.log(error);
+    infoTextEl.innerText = `An error showing, try again later`;
+  }
+}
+
+dictionaryCreate();
