425e02580936328b8cfc47cbe9f98205f9a4e8ed
unknown <s36651786@gmail.com>
2023-05-13 12:24:11 +0600

    another commit

--- Little project/English dictonary/script.js
+++ Little project/English dictonary/script.js
@@ -6,24 +6,38 @@ const inputEl = document.getElementById('input'),
   audioEl = document.getElementById('audio');
 
 inputEl.addEventListener('keyup', (event) => {
-  if (event.key === 'Enter') {
+  if (event.target.value && event.key === 'Enter') {
     dictionaryCreate(event.target.value);
   }
 });
 
 async function dictionaryCreate(word) {
   try {
+    infoTextEl.style.display = 'block';
+    meaningContainerEl.style.display = 'none';
+    infoTextEl.innerText = `Searching the meaning of "${word}"`;
+
     const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`,
       resultElement = await fetch(url).then((data) => data.json());
 
     if (resultElement.title) {
-      console.log('find it');
+      meaningContainerEl.style.display = 'block';
+      infoTextEl.style.display = 'none';
+
+      titleEl.innerText = word;
+      meaningEl.innerText = 'N/A';
+      audioEl.style.display = 'none';
     } else {
+      meaningContainerEl.style.display = 'block';
+      infoTextEl.style.display = 'none';
+
+      titleEl.innerText = resultElement[0].word;
+      meaningEl.innerText =
+        resultElement[0].meanings[0].definitions[0].definition;
+      audioEl.innerText = resultElement[0].phonetics[0].audio;
     }
   } catch (error) {
     console.log(error);
     infoTextEl.innerText = `An error showing, try again later`;
   }
 }
-
-dictionaryCreate();
