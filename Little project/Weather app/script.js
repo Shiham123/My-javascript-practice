e929416032622573dbe49341f1954f6201586ab5
unknown <s36651786@gmail.com>
2023-05-15 21:56:40 +0600

    ALHAMDULLILLAH MALIK

--- Little project/Weather app/script.js
+++ Little project/Weather app/script.js
@@ -1,42 +1,37 @@
-const weatherDataEl = document.getElementById('weather-data'),
+const formEl = document.querySelector('form'),
   cityInputEl = document.getElementById('city-input'),
-  formEl = document.querySelector('form');
+  weatherDataEl = document.getElementById('weather-data');
 
 const apiKey = `46f80a02ecae410460d59960ded6e1c6`;
 
 formEl.addEventListener('submit', (event) => {
   event.preventDefault();
-  const cityValue = cityInputEl.value;
-  // cityInputEl.value = '';
-
-  getWeather(cityValue);
+  const cityName = cityInputEl.value;
+  getWeatherData(cityName);
 });
 
-async function getWeather(cityValue) {
+async function getWeatherData(cityName) {
   try {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apiKey}&units=metric`,
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`,
       response = await fetch(url),
       data = await response.json();
 
-    const temperature = Math.round(data.main.temp),
+    const icon = data.weather[0].icon,
+      temperature = Math.round(data.main.temp),
       description = data.weather[0].description,
-      icon = data.weather[0].icon,
       details = [
-        `Feels like : ${Math.round(data.main.feels_like)}°C`,
-        `Humidity : ${data.main.humidity}%`,
-        `Wind speed : ${data.wind.speed} m/s`,
+        `Feels like: ${Math.round(data.main.feels_like)}°C`,
+        `Humidity: ${data.main.humidity}%`,
+        `Wind speed: ${data.wind.speed}`,
       ];
 
     weatherDataEl.querySelector(
       '.icon'
-    ).innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}.png" alt="weather icon"/>`;
-
+    ).innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}.png"/>`;
     weatherDataEl.querySelector(
       '.temperature'
     ).textContent = `${temperature}°C`;
-
     weatherDataEl.querySelector('.description').textContent = `${description}`;
-
     weatherDataEl.querySelector('.details').innerHTML = details
       .map((detail) => `<div>${detail}</div>`)
       .join('');
@@ -44,8 +39,8 @@ async function getWeather(cityValue) {
     console.log(error);
     weatherDataEl.querySelector('.icon').innerHTML = '';
     weatherDataEl.querySelector('.temperature').textContent = '';
-    weatherDataEl.querySelector('.description').textContent =
-      'An error happened, please try again later';
-    weatherDataEl.querySelector('.details').innerHTML = '';
+    weatherDataEl.querySelector('.description').textContent = '';
+    weatherDataEl.querySelector('.details').innerHTML =
+      'An error happened, try valid City name!';
   }
 }
