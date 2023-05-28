74efdb1f03823abb70380e3a63a4d27f91521da2
Shiham123 <s36651786@gmail.com>
2023-05-28 11:40:00 +0600

    running

---
+++ Big project/Trouble website/script.js
@@ -0,0 +1,25 @@
+const navItemOneEl = document.querySelector('.nav-item-one'),
+  navItemTwoEl = document.querySelector('.nav-item-two'),
+  navItemThreeEl = document.querySelector('.nav-item-three'),
+  navItemFourEl = document.querySelector('.nav-item-four');
+
+const itemOneEl = document.querySelector('.item-one'),
+  itemTwoEl = document.querySelector('.item-two'),
+  itemThreeEl = document.querySelector('.item-three'),
+  itemFourEl = document.querySelector('.item-four');
+
+navItemOneEl.addEventListener('click', () => {
+  itemOneEl.classList.toggle('active');
+});
+
+navItemTwoEl.addEventListener('click', () => {
+  itemTwoEl.classList.toggle('active');
+});
+
+navItemThreeEl.addEventListener('click', () => {
+  itemThreeEl.classList.toggle('active');
+});
+
+navItemFourEl.addEventListener('click', () => {
+  itemFourEl.classList.toggle('active');
+});
